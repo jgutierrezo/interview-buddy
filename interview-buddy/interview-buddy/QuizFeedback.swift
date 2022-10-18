f2de6399c55a8bd7bca242529457efa25d6a3bc0
jgutierrezo <jgutierrezoa2@gmail.com>
2022-10-18 13:19:06 -0400

    quiz feedback screen innitial commit

---
+++ interview-buddy/interview-buddy/QuizFeedback.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interview-buddy
+//
+//  Created by Juan David Gutierrez Ochoa on 2022-10-18.
+//
+
+import UIKit
+
+class QuizFeedback: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

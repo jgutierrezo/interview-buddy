34effc7dd87dcdde88b06e24281efbc10c29b461
jgutierrezo <jgutierrezoa2@gmail.com>
2022-10-18 13:19:33 -0400

    quiz feedback screen innitial commit

--- interview-buddy/interview-buddy/QuizFeedback.swift
+++ interview-buddy/interview-buddy/QuizFeedback.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class QuizFeedback: UIViewController {
 
+    @IBOutlet weak var rePlayButton: UIButton!
+    
+    @IBOutlet weak var homeButton: UIButton!
+    
+    @IBOutlet weak var topicsToReviewTV: UITextView!
+    @IBOutlet weak var correctTV: UITextView!
+    @IBOutlet weak var timedOutTV: UITextView!
+    @IBOutlet weak var mistakesTV: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rePlayButton.layer.cornerRadius = 15
+        rePlayButton.clipsToBounds = true
+        
+        homeButton.layer.cornerRadius = 15
+        homeButton.clipsToBounds = true
+        
+        correctTV.layer.cornerRadius = 10
+        mistakesTV.layer.cornerRadius = 10
+        timedOutTV.layer.cornerRadius = 10
+        topicsToReviewTV.layer.cornerRadius = 10
+
     }
 
 

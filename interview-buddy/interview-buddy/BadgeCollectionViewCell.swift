61735a79cec01d5ece8a4d8a3ccebfd4847fd88c
Rodrigo Chávez <rodrichavezm@gmail.com>
2022-11-08 17:09:07 -0500

    badges

---
+++ interview-buddy/interview-buddy/BadgeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BadgeCollectionViewCell.swift
+//  interview-buddy
+//
+//  Created by Rodrigo Chavez on 2022-11-08.
+//
+
+import UIKit
+
+class BadgeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var badgeImage: UIImageView!
+    
+}
